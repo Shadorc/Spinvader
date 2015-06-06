616de13edbc3708671f7260db104b0651cf397bd
Shadorc <bi-crosseur@hotmail.fr>
2015-06-06 13:03:13 +0200

    Remove multiple same sprites

--- src/me/shadorc/spinvader/graphic/Sprite.java
+++ src/me/shadorc/spinvader/graphic/Sprite.java
@@ -21,7 +21,7 @@ public class Sprite {
 
 		int width = 50;
 		int height = 40;
-		int column = Game.rand(8);
+		int column = Game.rand(7);
 		int row = Game.rand(16);
 
 		bu_img = bu_img.getSubimage((column*width + 15*column), (row*height + 10*row), 55, 40);
